aa967abb9c4df18eff5c536902e5f9be8dcc95c0
Daan <daanmeijers@live.nl>
2019-02-26 21:24:57 +0100

    Finished very basic modal for creation, can be easily modified for editing

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,30 +3,37 @@
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">NavBar</b-navbar-brand>
       <b-navbar-nav class="ml-auto">
-        <b-button size="md" type="submit">Create Recipe</b-button>
+        <b-button size="md" @click="showCreateForm">Create Recipe</b-button>
       </b-navbar-nav>
     </b-navbar>
 
     <Recipes v-bind:recipes="recipes"/>
 
+    <!-- MODALS -->
+    <RecipeForm ref="recipeform"/>
+
+    <!-- Modal Test -->
     <b-button v-b-modal.modal1>Launch demo modal</b-button>
 
     <!-- Modal Component -->
     <b-modal id="modal1" title="BootstrapVue">
       <p class="my-4">Hello from modal!</p>
     </b-modal>
+
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Recipes from "./Recipes.vue";
+import RecipeForm from "./RecipeForm.vue";
 
 export default {
   name: "Index",
   data() {
     return {
-      recipes: null
+      recipes: null,
+      recipeShow
     };
   },
   mounted() {
@@ -34,8 +41,14 @@ export default {
       .get("http://localhost:3000/recipes")
       .then(response => (this.recipes = response.data));
   },
+  methods: {
+    showCreateForm() {
+      this.$refs.recipeform.showModal();
+    }
+  },
   components: {
-    Recipes
+    Recipes,
+    RecipeForm,
   }
 };
 </script>
---
+++ src/components/RecipeForm.vue
@@ -0,0 +1,81 @@
+<template>
+    <b-modal
+        ref="recipeFormModal"
+        id="recipeForm"
+        title="Create Recipe"
+        >
+        <b-form @submit="onSubmit" @reset="onClear">
+            <b-form-group
+                id="formTitle"
+                label="Title"
+                label-for="formTitleInput"
+            >
+                <b-form-input
+                    id="formTitleInput"
+                    type="text"
+                    v-model="recipe.title"
+                    required
+                    placeholder="Title"
+                />
+            </b-form-group>
+
+            <b-form-group
+                id="formIngredient"
+                label="Ingredients"
+                label-for="formIngredientInput"
+                description="separate ingredients by ,"
+                required
+            >
+                <b-form-input
+                    id="formTitleInput"
+                    type="text"
+                    v-model="recipe.ingredients"
+                    required
+                    placeholder="Ingredients"
+                />
+            </b-form-group>
+
+            <b-form-group
+                id="formDescription"
+                label="Description"
+                label-for="formDescriptionInput"
+            >
+            <b-form-input
+                    id="formDescriptionInput"
+                    type="text"
+                    v-model="recipe.description"
+                    placeholder="Description"
+                />
+            </b-form-group>
+
+            <!-- <b-button type="submit" variant="primary">Create</b-button>
+            <b-button type="reset" variant="danger">Clear</b-button> -->
+        </b-form>
+    </b-modal>
+ </template>
+
+<script>
+export default {
+  name: 'RecipeForm',
+  data() {
+    return {
+      recipe: {
+        title: '',
+        ingredients: '',
+        description: '',
+      },
+    };
+  },
+  methods: {
+    showModal() {
+      this.$refs.recipeFormModal.show();
+    },
+    hideModal() {
+      this.$refs.recipeFormModal.hide();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
